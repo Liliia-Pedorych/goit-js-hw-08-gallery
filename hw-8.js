1d7788b5283f7c85ff84a453aec060f76c3a6ebc
Liliia-Pedorych <66588889+Liliia-Pedorych@users.noreply.github.com>
2020-11-19 21:51:07 +0200

    Создание и рендер разметки по массиву данных и предоставленному шаблону.

--- hw-8.js
+++ hw-8.js
@@ -1,6 +1,36 @@
-
+import galery from './gallery-items.js';
 
 // Создание и рендер разметки по массиву данных и предоставленному шаблону.
+
+
+const createGalleryItem = image => {
+
+    const itemListRef = document.createElement('li');
+    itemListRef.classList.add("gallery__item");
+
+    const refListRef = document.createElement('a');
+    refListRef.classList.add("gallery__link");
+    refListRef.href = image.original;
+
+    const imageRef = document.createElement('img');
+    imageRef.classList.add("gallery__image");
+    imageRef.src = image.preview;
+    imageRef.setAttribute("data-source", image.original);
+    imageRef.alt = image.description;
+    // imageRef.textContent = image.description;
+
+    refListRef.appendChild(imageRef);    
+
+    itemListRef.appendChild(refListRef);
+
+    return itemListRef;
+};
+
+const createGallery = galery.map(image => createGalleryItem(image));
+
+const galleryListRef = document.querySelector('.js-gallery');
+galleryListRef.append(...createGallery);
+
 
 // Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
 
